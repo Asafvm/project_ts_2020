63bfa87ce058d0dfd61070bb743306f589b5c622
Asafvm <37274017+Asafvm@users.noreply.github.com>
2020-08-01 23:02:01 +0300

    cosmetics

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,6 +1,5 @@
-import 'package:cloud_functions/cloud_functions.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:teamshare/providers/authentication.dart';
 import 'package:teamshare/screens/team_create_screen.dart';
 
 class MainScreen extends StatefulWidget {
@@ -19,34 +18,43 @@ class _MainScreenState extends State<MainScreen> {
           title: Text("Team Share"),
         ),
         //drawer: CustomDrawer(),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("You are not part of a team... yet"),
-              RaisedButton.icon(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(builder: (_) => TeamCreateScreen()),
-                  );
-                },
-                icon: Icon(Icons.create),
-                label: Text("Create"),
-              ),
-              Text("Your team now!"),
-              FloatingActionButton(
-                  onPressed: () async => {
-                        CloudFunctions.instance
-                            .getHttpsCallable(functionName: "findTeam")
-                            .call(<String, dynamic>{
-                              "user": await Authentication().userEmail
-                            })
-                            .then((value) => print(value.data))
-                            .catchError((e) => print("Error: ${e.toString()}"))
-                      })
-            ],
-          ),
-        ),
+        body: StreamBuilder<Map<String, dynamic>>(
+            stream: Firestore.instance
+                .collection("userEmail")
+                .document("ts@ts.com")
+                .get()
+                .then((value) => value.data)
+                .asStream(),
+            builder: (context, snapshot) {
+              if (snapshot == null || snapshot.data == null) {
+                return Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("You are not part of a team... yet"),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: RaisedButton.icon(
+                          onPressed: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                  builder: (_) => TeamCreateScreen()),
+                            );
+                          },
+                          icon: Icon(Icons.create),
+                          label: Text("Create"),
+                        ),
+                      ),
+                      Text("Your team now!"),
+                    ],
+                  ),
+                );
+              } else {
+                return Center(
+                  child: Text(snapshot.data.toString()),
+                );
+              }
+            }),
       ),
     );
   }
