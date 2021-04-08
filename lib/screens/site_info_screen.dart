e1c3e64c503858036510faeea565f3b0d762d64a
Asafvm <37274017+Asafvm@users.noreply.github.com>
2021-04-08 22:26:11 +0300

    improved site info screen. added navigation

--- lib/screens/site_info_screen.dart
+++ lib/screens/site_info_screen.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:location/location.dart';
+import 'package:pdf/widgets.dart' as widgets;
 import 'package:teamshare/helpers/location_helper.dart';
 import 'package:teamshare/models/site.dart';
-import 'package:teamshare/providers/applogger.dart';
 import 'package:teamshare/providers/consts.dart';
-import 'package:teamshare/screens/map_screen.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SiteInfoScreen extends StatefulWidget {
   final Site site;
@@ -43,58 +41,80 @@ class _SiteInfoScreenState extends State<SiteInfoScreen> {
       ),
       body: Column(
         children: [
-          Flexible(
-            flex: 4,
-            fit: FlexFit.tight,
-            child: Row(
-              children: [
-                Flexible(
-                  flex: 4,
-                  fit: FlexFit.tight,
-                  child: Column(
-                    children: [
-                      Expanded(
-                        child: Container(
-                          padding: EdgeInsets.all(2),
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                              width: 1,
-                              color: Colors.grey,
-                            ),
-                          ),
-                          child: _previewImageUrl == null
-                              ? Text('Choose a site')
-                              : Image.network(
-                                  _previewImageUrl,
-                                  fit: BoxFit.fill,
-                                ),
+          Column(
+            children: [
+              Text(
+                widget.site.name,
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Stack(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(2),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        width: 1,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    child: Image.network(
+                      _previewImageUrl,
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 20,
+                    right: 20,
+                    child: Container(
+                      padding: const EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                          border: Border.all(width: 1, color: Colors.black),
+                          borderRadius: BorderRadius.circular(15),
+                          color: Colors.white),
+                      child: Material(
+                        child: Row(
+                          children: [
+                            IconButton(
+                                splashColor: Theme.of(context).accentColor,
+                                splashRadius: 20,
+                                iconSize: 35,
+                                color: Theme.of(context).primaryColor,
+                                icon:
+                                    Image.asset('assets/icons/googlemaps.png'),
+                                tooltip: 'Directions',
+                                onPressed: () => launchGoogleMaps(
+                                    widget.site.address.lat,
+                                    widget.site.address.lng)),
+                            IconButton(
+                                splashColor: Theme.of(context).accentColor,
+                                splashRadius: 20,
+                                iconSize: 35,
+                                color: Theme.of(context).primaryColor,
+                                icon: Image.asset('assets/icons/waze.png'),
+                                tooltip: 'Directions',
+                                onPressed: () => launchWaze(
+                                    widget.site.address.lat,
+                                    widget.site.address.lng)),
+                          ],
                         ),
                       ),
-                      IconButton(
-                        icon: Icon(Icons.directions),
-                        tooltip: 'Directions',
-                        onPressed: null, //TODO: open maps or waze
-                      ),
-                    ],
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Text(
+                  widget.site.address.toString(),
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-                Flexible(
-                    flex: 6,
-                    fit: FlexFit.tight,
-                    child: Padding(
-                      padding: const EdgeInsets.all(15.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('Name'),
-                          Text('City'),
-                          Text('Street'),
-                        ],
-                      ),
-                    )),
-              ],
-            ),
+              ),
+            ],
           ),
           Flexible(
             flex: 7,
@@ -133,5 +153,35 @@ class _SiteInfoScreenState extends State<SiteInfoScreen> {
         ],
       ),
     );
+  }
+
+  void launchWaze(double lat, double lng) async {
+    var url = 'waze://?ll=${lat.toString()},${lng.toString()}';
+    var fallbackUrl =
+        'https://waze.com/ul?ll=${lat.toString()},${lng.toString()}&navigate=yes';
+    try {
+      bool launched =
+          await launch(url, forceSafariVC: false, forceWebView: false);
+      if (!launched) {
+        await launch(fallbackUrl, forceSafariVC: false, forceWebView: false);
+      }
+    } catch (e) {
+      await launch(fallbackUrl, forceSafariVC: false, forceWebView: false);
+    }
+  }
+
+  void launchGoogleMaps(double lat, double lng) async {
+    var url = 'google.navigation:q=${lat.toString()},${lng.toString()}';
+    var fallbackUrl =
+        'https://www.google.com/maps/search/?api=1&query=${lat.toString()},${lng.toString()}';
+    try {
+      bool launched =
+          await launch(url, forceSafariVC: false, forceWebView: false);
+      if (!launched) {
+        await launch(fallbackUrl, forceSafariVC: false, forceWebView: false);
+      }
+    } catch (e) {
+      await launch(fallbackUrl, forceSafariVC: false, forceWebView: false);
+    }
   }
 }
