bf4fee7678e48ac9a44de000ce7a5108ad4a18df
Asafvm <37274017+Asafvm@users.noreply.github.com>
2021-05-13 12:51:24 +0300

    bugfix in pdf creation

--- lib/widgets/custom_field.dart
+++ lib/widgets/custom_field.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:teamshare/models/field.dart';
-import 'package:auto_size_text/auto_size_text.dart';
 
 class CustomField extends StatefulWidget {
   final Field field;
   final MediaQueryData mqd;
   final Function editFunction;
   final Size pdfSizeOnScreen;
+  final double appbarHeight;
   final double scale;
   final Offset focalPoint;
-  CustomField(this.field, this.mqd, this.editFunction, this.pdfSizeOnScreen,
-      this.scale, this.focalPoint);
+  CustomField(
+      {this.field,
+      this.mqd,
+      this.editFunction,
+      this.pdfSizeOnScreen,
+      this.scale,
+      this.focalPoint,
+      this.appbarHeight});
 
   @override
   _CustomFieldState createState() => _CustomFieldState();
 }
+
+const ballDiameter = 8.0;
 
 class _CustomFieldState extends State<CustomField> {
   bool _selected = false;
@@ -23,21 +31,18 @@ class _CustomFieldState extends State<CustomField> {
   Widget build(BuildContext context) {
     Size rectSize = widget.field.size; // * widget.scale;
 
-    Widget _createRect(Color color, double width, Size size) {
-      return Container(
-        height: size.height,
-        width: size.width,
-        decoration: BoxDecoration(
-          border: Border.all(width: width, color: color),
-        ),
-        child: AutoSizeText(
-          '(${widget.field.index.toString()})' + widget.field.defaultValue,
-          minFontSize: 0,
-          stepGranularity: 0.1,
-        ),
-      );
-    }
-
+    var field = Container(
+      height: rectSize.height,
+      width: rectSize.width,
+      decoration: BoxDecoration(
+        border: Border.all(width: 1, color: Colors.green),
+      ),
+      child: Text(
+        '(${widget.field.index.toString()})' + widget.field.defaultValue,
+        style: TextStyle(fontSize: rectSize.height - 2),
+        softWrap: true,
+      ),
+    );
     return Positioned(
       height: rectSize.height,
       width: rectSize.width,
@@ -46,27 +51,84 @@ class _CustomFieldState extends State<CustomField> {
       left: widget.field.offset.dx *
           widget.pdfSizeOnScreen.width, // / widget.scale,
       child: GestureDetector(
+        behavior: HitTestBehavior.opaque,
         onTap: () => setState(() {
           _selected = !_selected;
-          if (_selected) {
-            widget.editFunction(context, widget.field);
-          }
         }),
-        onLongPress:
-            () {}, //ignore long press TODO:change long press to drag with animation
         onPanUpdate: (details) => {if (_selected) _resize(details.delta)},
+        onPanEnd: (details) => {
+          setState(() {
+            _selected = false;
+          })
+        },
+        onLongPress: () => widget.editFunction(context, widget.field),
         child: _selected
-            ? _createRect(Colors.deepOrange, 2, rectSize)
+            ? Stack(clipBehavior: Clip.none, children: [
+                field,
+                Positioned(
+                  top: 0,
+                  left: 0,
+                  child: Container(
+                    width: ballDiameter,
+                    height: ballDiameter,
+                    decoration: BoxDecoration(
+                      color: Colors.blue.withOpacity(0.5),
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 0,
+                  right: 0,
+                  child: Container(
+                    width: ballDiameter,
+                    height: ballDiameter,
+                    decoration: BoxDecoration(
+                      color: Colors.blue.withOpacity(0.5),
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  bottom: 0,
+                  left: 0,
+                  child: Container(
+                    width: ballDiameter,
+                    height: ballDiameter,
+                    decoration: BoxDecoration(
+                      color: Colors.blue.withOpacity(0.5),
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  bottom: 0,
+                  right: 0,
+                  child: Container(
+                    width: ballDiameter,
+                    height: ballDiameter,
+                    decoration: BoxDecoration(
+                      color: Colors.blue.withOpacity(0.5),
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                ),
+              ])
             : Draggable(
                 data: widget.field.index,
                 onDragEnd: (details) {
                   Offset newPos = Offset(
-                      details.offset.dx / widget.pdfSizeOnScreen.width,
-                      details.offset.dy / widget.pdfSizeOnScreen.height);
+                      (details.offset.dx - widget.mqd.viewPadding.topLeft.dx) /
+                          widget.pdfSizeOnScreen.width,
+                      (details.offset.dy -
+                              widget.mqd.padding.top -
+                              widget
+                                  .appbarHeight) / //75 =  empty space due to centered widget
+                          widget.pdfSizeOnScreen.height);
                   _relocate(newPos);
                 },
-                feedback: _createRect(Colors.orange, 1, rectSize),
-                child: _createRect(Colors.green, 1, rectSize),
+                feedback: Card(elevation: 5, child: field),
+                child: field,
                 childWhenDragging: Container(),
               ),
       ),
@@ -74,15 +136,17 @@ class _CustomFieldState extends State<CustomField> {
   }
 
   void _relocate(Offset offset) {
-    Offset viewRatio = Offset(
-        widget.mqd.viewPadding.topLeft.dx / widget.pdfSizeOnScreen.width,
-        widget.mqd.viewPadding.topLeft.dy / widget.pdfSizeOnScreen.height);
+    // Offset viewRatio = Offset(
+    //     widget.mqd.viewPadding.topLeft.dx / widget.pdfSizeOnScreen.width,
+    //     widget.mqd.viewPadding.topLeft.dy / widget.pdfSizeOnScreen.height);
 
-    Offset factor =
-        Offset(0, 57 / widget.pdfSizeOnScreen.height); //TODO: figure this out
+    // Offset factor =
+    //     Offset(0, 50 / widget.pdfSizeOnScreen.height); //TODO: figure this out
 
     setState(() {
-      widget.field.offset = offset - viewRatio - factor;
+      widget.field.offset = offset;
+      // -viewRatio;
+      // -factor;
     });
   }
 
