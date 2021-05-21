bb8c0c316e541eeabb69ad33b6c263fb22fe2451
Asafvm <37274017+Asafvm@users.noreply.github.com>
2021-05-21 20:21:56 +0300

    file explorer fixes

--- lib/screens/drawer_screens/FileExplorer.dart
+++ lib/screens/drawer_screens/FileExplorer.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:teamshare/helpers/decoration_library.dart';
+import 'package:teamshare/helpers/firebase_paths.dart';
 import 'package:teamshare/screens/pdf/pdf_viewer_page.dart';
 import 'package:share/share.dart';
 
@@ -14,166 +15,241 @@ class FileExplorer extends StatefulWidget {
 }
 
 class _FileExplorerState extends State<FileExplorer> {
+  Future<Directory> currentPath;
   bool _multiSelect = false;
   List<bool> _selectedList = [];
-  List<FileSystemEntity> files;
+  static List<FileSystemEntity> files;
+  List<FileSystemEntity> filteredFiles;
+  bool _isFolder = false;
 
   String _textFilter = '';
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      currentPath = widget.path;
+      filteredFiles = files;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        actions: [
-          if (_multiSelect)
-            IconButton(
-                icon: Icon(Icons.share),
-                onPressed: () {
-                  Share.shareFiles(files
-                      .where((element) => _selectedList[files.indexOf(element)])
-                      .map((e) => e.path)
-                      .toList());
-                }),
-          if (_multiSelect)
-            IconButton(
-                icon: Icon(Icons.delete),
-                onPressed: () {
-                  showDialog(
-                    context: context,
-                    builder: (context) => AlertDialog(
-                      title: Text(
-                          'Delete ${_selectedList.where((element) => element).length} Files?'),
-                      content:
-                          Text('This action cannot be reversed\nAre you sure?'),
-                      actions: [
-                        TextButton(
-                            onPressed: () {
-                              files
-                                  .where((element) =>
-                                      _selectedList[files.indexOf(element)])
-                                  .toList()
-                                  .forEach((element) {
-                                element.delete();
-                              });
-
-                              setState(() {
-                                _multiSelect = false;
-                              });
-                              Navigator.pop(context);
-                            },
-                            child: Text('OK')),
-                        TextButton(
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                            child: Text('Cancel'))
-                      ],
-                    ),
-                  );
-                })
-        ],
-      ),
-      body: FutureBuilder<Directory>(
-        future: widget.path,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            files = snapshot.data
-                .listSync(); //list of SystemFileEntities (file,folders,etc)
-            //sort folders first
-            files.sort((a, b) => a
-                .statSync()
-                .type
-                .toString()
-                .compareTo(b.statSync().type.toString()));
-
-            return Column(
-              children: [
-                TextField(
-                  decoration: DecorationLibrary.searchDecoration(
-                      context: context, hint: 'Search'),
-                  onChanged: (value) => setState(() {
-                    _textFilter = value;
+    return WillPopScope(
+      onWillPop: _handleBackButton,
+      child: Scaffold(
+        appBar: AppBar(
+          actions: [
+            if (_multiSelect)
+              IconButton(
+                  icon: Icon(Icons.share),
+                  onPressed: () {
+                    Share.shareFiles(files
+                        .where(
+                            (element) => _selectedList[files.indexOf(element)])
+                        .map((e) => e.path)
+                        .toList());
                   }),
-                ),
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: files //filter by search text
-                        .where((element) => element.path
-                            .substring(element.path.lastIndexOf('/') + 1)
-                            .toLowerCase()
-                            .contains(_textFilter.toLowerCase()))
-                        .toList()
-                        .length,
-                    shrinkWrap: true,
-                    itemBuilder: (context, index) {
-                      FileSystemEntity file = files[index];
-                      FileStat fileStats = file.statSync();
-                      return _multiSelect
-                          ? CheckboxListTile(
-                              value: _selectedList[index],
-                              onChanged: (value) {
-                                setState(() {
-                                  _selectedList[index] = value;
-                                  if (_selectedList
-                                          .where((element) => element == true)
-                                          .length ==
-                                      0)
-                                    _multiSelect =
-                                        false; //disable multiselect on empty selected list
+            if (_multiSelect)
+              IconButton(
+                  icon: Icon(Icons.delete),
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                        title: Text(
+                            'Delete ${_selectedList.where((element) => element).length} Files?'),
+                        content: Text(
+                            'This action cannot be reversed\nAre you sure?'),
+                        actions: [
+                          TextButton(
+                              onPressed: () {
+                                files
+                                    .where((element) =>
+                                        _selectedList[files.indexOf(element)])
+                                    .toList()
+                                    .forEach((element) {
+                                  element.delete();
                                 });
-                              },
-                              title: Text(file.path.substring(
-                                  files[index].path.lastIndexOf('/') + 1)),
-                              subtitle: Text('${fileStats.modified.toLocal()}'),
-                              secondary: Icon(
-                                  fileStats.type.toString() == "file"
-                                      ? Icons.file_copy
-                                      : Icons.folder),
-                            )
-                          : ListTile(
-                              onLongPress: () {
-                                _initSelectedList(files.length);
+
                                 setState(() {
-                                  _multiSelect = true;
-                                  _selectedList[index] =
-                                      true; //mark first selected
+                                  _multiSelect = false;
                                 });
+                                Navigator.pop(context);
                               },
-                              onTap: () {
-                                //open if has pdf extension
-                                if (files[index].path.endsWith(".pdf"))
-                                  Navigator.of(context).push(MaterialPageRoute(
-                                    builder: (context) => PDFScreen(
-                                        fields: [],
-                                        instrumentID: "",
-                                        onlyFields: true,
-                                        pathPDF: files[index].path),
-                                  ));
-
-                                //TODO: else if folder call FileExplorer(currect path + folder name])
-
-                                else if (files[index].path.endsWith(".")) {}
+                              child: Text('OK')),
+                          TextButton(
+                              onPressed: () {
+                                Navigator.pop(context);
                               },
+                              child: Text('Cancel'))
+                        ],
+                      ),
+                    );
+                  })
+          ],
+        ),
+        body: FutureBuilder<Directory>(
+          future: currentPath,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              print(snapshot.data.path);
+              files = snapshot.data
+                  .listSync(); //list of SystemFileEntities (file,folders,etc)
+              //sort folders first
+              files.sort((a, b) => a
+                  .statSync()
+                  .type
+                  .toString()
+                  .compareTo(b.statSync().type.toString()));
 
-                              leading: Icon(fileStats.type.toString() == "file"
-                                  ? Icons.file_copy
-                                  : Icons.folder), //set icon
-                              title: Text(file.path.substring(
-                                  files[index].path.lastIndexOf('/') + 1)),
-                              subtitle: Text('${fileStats.modified.toLocal()}'),
-                            );
-                    },
+              return Column(
+                children: [
+                  TextField(
+                    decoration: DecorationLibrary.searchDecoration(
+                        context: context, hint: 'Search'),
+                    onChanged: (value) => setState(() {
+                      _textFilter = value;
+
+                      // filteredFiles
+                      //     .where((element) => element.path
+                      //         .substring(element.path.lastIndexOf('/') + 1)
+                      //         .toLowerCase()
+                      //         .contains(value.toLowerCase()))
+                      //     .toList();
+                    }),
                   ),
-                ),
-              ],
-            );
-          } else
-            return Container();
-        },
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: files //filter by search text
+                          .where((element) => element.path
+                              .substring(element.path.lastIndexOf('/') + 1)
+                              .toLowerCase()
+                              .contains(_textFilter.toLowerCase()))
+                          .toList()
+                          .length,
+                      shrinkWrap: true,
+                      itemBuilder: (context, index) {
+                        if (files[index].path.endsWith("")) {
+                          _isFolder = true;
+                        }
+
+                        FileSystemEntity file = files[index];
+                        FileStat fileStats = file.statSync();
+                        return _multiSelect
+                            ? CheckboxListTile(
+                                value: _selectedList[index],
+                                onChanged:
+                                    //onChangedItem(index, _selectedList[index]),
+                                    (value) {
+                                  setState(() {
+                                    _selectedList[index] = value;
+                                    if (_selectedList
+                                            .where((element) => element == true)
+                                            .length ==
+                                        0)
+                                      _multiSelect =
+                                          false; //disable multiselect on empty selected list
+                                  });
+                                },
+                                title: Text(file.path.substring(
+                                    files[index].path.lastIndexOf('/') + 1)),
+                                subtitle:
+                                    Text('${fileStats.modified.toLocal()}'),
+                                secondary: Icon(
+                                    fileStats.type.toString() == "file"
+                                        ? Icons.file_copy
+                                        : Icons.folder),
+                              )
+                            : ListTile(
+                                onLongPress: () {
+                                  _initSelectedList(files.length);
+                                  setState(() {
+                                    _multiSelect = true;
+                                    _selectedList[index] =
+                                        true; //mark first selected
+                                  });
+                                },
+                                onTap: () {
+                                  //if it's a folder
+                                  if (files[index].statSync().type ==
+                                      FileSystemEntityType.directory) {
+                                    Future<Directory> folderPath =
+                                        Directory(files[index].path)
+                                            .create(recursive: true);
+                                    Navigator.of(context)
+                                        .push(MaterialPageRoute(
+                                      builder: (context) => FileExplorer(
+                                        path: folderPath,
+                                      ),
+                                    ));
+                                  }
+                                  //open if has pdf extension
+                                  if (files[index].statSync().type ==
+                                          FileSystemEntityType.file &&
+                                      files[index].path.endsWith(".pdf"))
+                                    Navigator.of(context)
+                                        .push(MaterialPageRoute(
+                                      builder: (context) => PDFScreen(
+                                          fields: [],
+                                          instrumentID: "",
+                                          onlyFields: true,
+                                          pathPDF: files[index].path),
+                                    ));
+                                },
+
+                                leading: Icon(
+                                    fileStats.type.toString() == "file"
+                                        ? Icons.file_copy
+                                        : Icons.folder), //set icon
+                                title: Text(file.path.substring(
+                                    files[index].path.lastIndexOf('/') + 1)),
+                                subtitle:
+                                    Text('${fileStats.modified.toLocal()}'),
+                              );
+                      },
+                    ),
+                  ),
+                ],
+              );
+            } else
+              return Container();
+          },
+        ),
       ),
     );
   }
 
   void _initSelectedList(int length) {
     _selectedList = List<bool>.filled(length, false);
+  }
+
+  onChangedItem(int index, bool value) {
+    setState(() {
+      _selectedList[index] = value;
+      if (_selectedList.where((element) => element == true).length == 0)
+        _multiSelect = false; //disable multiselect on empty selected list
+
+      if (files[index].path.endsWith("")) {
+        return null;
+      }
+    });
+  }
+
+  Future<bool> _handleBackButton() async {
+    String root = await FirebasePaths.rootTeamFolder();
+    String current = (await widget.path).path;
+    print(current);
+    if (current == root)
+      Navigator.of(context).pop();
+    else
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => FileExplorer(
+              path: Directory(current.substring(0, current.lastIndexOf('/')))
+                  .create(),
+            ),
+          ));
   }
 }
